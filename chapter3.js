cec8d841af57564d2d7bcc7e0a4d756ed8042321
griff <nicholas.s.griffen@gmail.com>
2018-05-22 00:33:38 -0700

    resolve name clashes, properly track unused values, extract filter utility function, remove xtra used tracking

--- chapter3.js
+++ chapter3.js
@@ -1,19 +1,29 @@
 //adapted from insult generator challenge
+//I opted for string interpolation over the suggestions below, but for the sake of completeness
+//compliment =  [name, "your", quality, "makes", "me", feel, like, adjective, noun].join(" ");
+//compliment=  name + "your" + quality + "makes me feel like" + adjective + noun;
+
 function complimentGenerator() {
-  var pick, unused, used, buildCompliment;
-  pick = function(array) {
-    var index = Math.floor(Math.random() * array.length);
-    return array[index];
-  }
-  unused = {quality: ["sense of humor", "smile", "curiosity", "charm", "manners", "intelligence", "quick wit"],
+  //store globals in their own object to avoid name clashes - could also use let or const
+  var globals = {};
+  globals.unused = {quality: ["sense of humor", "kindness", "curiosity", "charm", "manners", "intelligence", "quick wit"],
             adjective: ["bright", "warm", "a fresh", "the world's greatest", "superlative", "my favorite", "the best"],
             noun: ["wildflowers", "hugs", "sunny day", "apple pie", "afternoon in the garden", "chicken soup", "cuppa joe"]
           };
-  used = {quality: [], adjective: [], noun: []};
-  buildCompliment = function(name = "Friend", unused, used) {
+  //utility functions
+  function pick(array) {
+    var index = Math.floor(Math.random() * array.length);
+    return array[index];
+  }
+  
+  function rmvKey(key) {
+    return key != this;
+  }
+  //main function
+  function buildCompliment(name = "Friend", unused, used) {
     var quality, adjective, noun, compliment;
     if (unused.quality.length === 0) {
-      unused = Object.assign({}, used);
+      unused = Object.assign({}, globals.unused);
     }
     quality = pick(unused.quality);
     adjective = pick(unused.adjective);
@@ -24,15 +34,12 @@ function complimentGenerator() {
       }
      compliment = `${name}, your ${quality} makes me feel like ${adjective} ${noun}`;
      window.alert(compliment);
-     used.quality.push(quality);
-     used.adjective.push(adjective);
-     used.noun.push(noun);
-     unused.quality = unused.quality.filter((qual => qual != this), quality);
-     unused.adjective = unused.adjective.filter((qual => qual != this), adjective);
-     unused.noun = unused.noun.filter((qual => qual != this), noun);
+     unused.quality = unused.quality.filter(rmvKey, quality);
+     unused.adjective = unused.adjective.filter(rmvKey, adjective);
+     unused.noun = unused.noun.filter(rmvKey, noun);
      buildCompliment(name, unused, used);
     }
   };
-  buildCompliment("Friend", unused, used);
-}
+  buildCompliment("Friend", Object.assign({}, globals.unused));
+} 
 complimentGenerator();
